34c13397945a77c983d9f9a6bcf91fbd1cf0ee79
Vlad Panazan <git@praznet.com>
2025-01-18 19:38:06 +0100

    nvim: weburl for github

---
+++ dot_config/nvim/lua/my/tests/executable_test_weburl.lua
@@ -0,0 +1,28 @@
+#!/usr/bin/env -S nvim -l
+
+local weburl = require 'my.weburl'
+
+local cases = {
+	{
+		remote = 'git@github.com:psnszsn/dotfiles.git',
+		baseurl = 'https://github.com/psnszsn/dotfiles/blob/main/dot_config/nvim/init.lua#L22-L24',
+	},
+	{
+		remote = 'code.com:hello/world',
+		baseurl = 'https://sg.uberinternal.com/code.com/uber-code/hello-world@main/-/blob/dot_config/nvim/init.lua?L22-L24',
+	},
+}
+
+local T = {}
+
+T['works'] = function()
+	for _, c in ipairs(cases) do
+		local result = weburl.__get_baseurl(c.remote, 'main', 'dot_config/nvim/init.lua', 22, 24)
+		assert(result == c.baseurl, result)
+	end
+end
+
+for name, func in pairs(T) do
+	vim.print(name .. '...')
+	func()
+end
--- dot_config/nvim/lua/my/weburl.lua
+++ dot_config/nvim/lua/my/weburl.lua
@@ -1,8 +1,62 @@
-local function get_web_url(args)
+---@param line1 integer
+---@param line2 integer
+local function get_linerange(line1, line2)
+	return line1 == line2 and string.format('L%d', line1) or string.format('L%d-L%d', line1, line2)
+end
+
+local rules = {
+	{
+		pattern = '^git@([^:]+):(.+)/(.+)%.git$',
+		format = function(host, user, repo, branch, filepath, line1, line2)
+			return string.format(
+				'https://%s/%s/%s/blob/%s/%s#%s',
+				host,
+				user,
+				repo,
+				branch,
+				filepath,
+				get_linerange(line1, line2)
+			)
+		end,
+	},
+	{
+		pattern = '^(.-):([^/]+)/(.+)$',
+		format = function(host, user, repo, branch, filepath, line1, line2)
+			return string.format(
+				'https://sg.uberinternal.com/%s/uber-code/%s-%s@%s/-/blob/%s?%s',
+				host,
+				user,
+				repo,
+				branch,
+				filepath,
+				get_linerange(line1, line2)
+			)
+		end,
+	},
+}
+table.insert(rules, {
+	pattern = '^https?://([^/]+)/([^/]+)/([^/]+)$',
+	format = rules[1].format,
+})
+
+---@param remote string
+---@param branch string
+local function get_web_url(remote, branch, filepath, line1, line2)
+	for _, rule in ipairs(rules) do
+		local captures = { remote:match(rule.pattern) }
+		if #captures ~= 0 then
+			table.insert(captures, branch)
+			table.insert(captures, filepath)
+			table.insert(captures, line1)
+			table.insert(captures, line2)
+			return rule.format(table.unpack(captures))
+		end
+	end
+end
+
+local function copy_web_url(args)
 	local filepath = vim.api.nvim_buf_get_name(0)
 	local directory = vim.fn.fnamemodify(filepath, ':h')
-	local line_range = args.line1 == args.line2 and string.format('L%d', args.line1)
-		or string.format('L%d-L%d', args.line1, args.line2)
 
 	local git_root_proc = vim.system({ 'git', 'rev-parse', '--show-toplevel' }, { cwd = directory }):wait()
 	if git_root_proc.code ~= 0 then
@@ -14,21 +68,19 @@ local function get_web_url(args)
 
 	local remote_url_proc = vim.system({ 'git', 'config', '--get', 'remote.origin.url' }, { cwd = directory }):wait()
 	if remote_url_proc.code ~= 0 then
-		vim.notify('No valid GitHub remote URL found: ' .. (remote_url_proc.stderr or ''), vim.log.levels.ERROR)
+		vim.notify('No valid remote URL found: ' .. (remote_url_proc.stderr or ''), vim.log.levels.ERROR)
 		return
 	end
 	local remote_url = vim.trim(remote_url_proc.stdout)
 
-	local transformed_url = remote_url:gsub('^(.-):([^/]+)/(.+)$', 'https://sg.uberinternal.com/%1/uber-code/%2-%3')
-
 	local proc = vim.system({ 'git', 'rev-parse', '--abbrev-ref', 'HEAD' }, { cwd = directory }):wait()
 	if proc.code ~= 0 then
-		vim.notify('Unable tget current branch: ' .. (proc.stderr or ''), vim.log.levels.ERROR)
+		vim.notify('Unable to get current branch: ' .. (proc.stderr or ''), vim.log.levels.ERROR)
 		return
 	end
 	local branch = vim.trim(proc.stdout)
 
-	local url = string.format('%s@%s/-/blob/%s?%s', transformed_url, branch, relative_path, line_range)
+	local url = get_web_url(remote_url, branch, relative_path, args.line1, args.line2)
 
 	vim.fn.setreg('+', url)
 
@@ -37,6 +89,10 @@ end
 
 vim.api.nvim_create_user_command(
 	'WebUrl',
-	get_web_url,
+	copy_web_url,
 	{ desc = 'Get the Web URL for the current line/range', range = true }
 )
+
+return {
+	__get_baseurl = get_web_url,
+}

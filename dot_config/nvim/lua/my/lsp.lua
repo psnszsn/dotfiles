0926b6e9cbc73ce194efbccadb5c7a31e03de165
psnszsn <535600+psnszsn@users.noreply.github.com>
2025-06-24 19:33:04 +0000

    style: format files

--- dot_config/nvim/lua/my/lsp.lua
+++ dot_config/nvim/lua/my/lsp.lua
@@ -24,7 +24,7 @@ vim.diagnostic.config {
 
 	-- Alternatively, customize specific options
 	virtual_lines = {
-	 -- Only show virtual line diagnostics for the current cursor line
-	 current_line = true,
+		-- Only show virtual line diagnostics for the current cursor line
+		current_line = true,
 	},
 }

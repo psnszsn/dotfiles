0dfae4a0a90a2db93fd96a0e98f6496d23efcfd7
Vlad Panazan <git@praznet.com>
2025-06-28 00:18:10 +0200

    remove awful hack

--- dot_config/nvim/lua/my/nested.lua
+++ dot_config/nvim/lua/my/nested.lua
@@ -1,7 +1,4 @@
 if not vim.env.NVIM then
-	function _G.IsBufnrClosed(bufnr)
-		return not vim.api.nvim_buf_is_valid(bufnr) or not vim.api.nvim_buf_is_loaded(bufnr)
-	end
 	return
 end
 
@@ -9,15 +6,8 @@ local sock = vim.fn.sockconnect('pipe', vim.env.NVIM, { rpc = true })
 
 local filepath = vim.api.nvim_buf_get_name(0)
 if filepath and filepath ~= '' then
-	vim.print(filepath)
-	vim.fn.rpcrequest(sock, 'nvim_command', 'edit ' .. vim.fn.fnameescape(filepath))
-
-	local bufnr = vim.fn.rpcrequest(sock, 'nvim_call_function', 'bufnr', { '%' })
-	print(bufnr)
-	while not vim.fn.rpcrequest(sock, 'nvim_exec_lua', 'return _G.IsBufnrClosed(...)', { bufnr }) do
-		-- print 'not closed'
-		vim.wait(100)
-	end
+	print(filepath)
+	vim.fn.rpcnotify(sock, 'nvim_command', 'edit ' .. vim.fn.fnameescape(filepath))
 end
 
 print 'nested nvim'
